dbe4ec3cca460852d7685b1c523bdf114983e58f
include-nan <zjn1210@outlook.com>
2020-12-13 14:30:09 +0800

    first commit

---
+++ 2.两数相加.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=cpp
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * */
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+   public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int flag = 0;
+        ListNode* p1 = l1,*p2 = l2;
+        while (p1->next != nullptr || p2->next!=nullptr) {
+            
+        }
+    }
+};
+// @lc code=end
